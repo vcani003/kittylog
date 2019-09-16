23486b2bafe571d616b1ce23b0d3d923dca461d4
vcani003 <veronicacanido@gmail.com>
2019-09-16 01:46:59 -0400

    Initializing kittylog react app :)

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import kitty from './images/kitty.gif'
+
+const App = () => {
+    return (
+        <>
+            <h1>Coco</h1>
+            <img src={kitty} alt='kitty' />
+        </>
+    )
+}
+
+export default App;
